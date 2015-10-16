c1ce5488f52c1cb7efd1a9c6c5b12bd0de9d4be3
Hank <hank.obrien900@icloud.com>
2015-10-15 18:26:45 -0700

    --

--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		System.out.println("Hank just added this");
 		System.out.println("and this");
+		int i;
 
 	}
 	
