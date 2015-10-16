c8e19dac6486e50028a087a2a695fc912dbe2c1c
Hank <hank.obrien900@icloud.com>
2015-10-16 09:23:33 -0700

    deleted junk

--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -12,24 +12,7 @@ public class Main extends Application {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Hank just added this");
-		System.out.println("and this");
-		int i;
 
-	}
-	
-	public static void test(){
-		
-		System.out.println("Hello World");
-	}
-	
-	private void foo() {
-		System.out.println("stuff");
-	}
-	
-	public void name() {
-		System.out.println("");
-		System.out.println("also");
 	}
 
 }
