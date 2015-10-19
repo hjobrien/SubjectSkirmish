924bf3982079b72b4f55669a73015cde2bb3b788
Liam Kreiss <liam.kreiss@gmail.com>
2015-10-19 06:44:33 -0700

    played around with the drawing panel

--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -44,21 +44,45 @@ public class Main extends Application {
 	public void addGraphicOutput(GridPane grid){
 		Canvas canvas = new Canvas(SCREEN_WIDTH - BORDER_WIDTH, SCREEN_HEIGHT - BORDER_HEIGHT);
 		GraphicsContext g = canvas.getGraphicsContext2D();
+		
 		//puts 0,0 in center of display
+		//are you sure this is the center? because it doesn't seem centered
 		g.scale(1,-1);
 		g.translate(0, (-1) * SCREEN_HEIGHT);
 		g.translate(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 		g.scale(50,50);
 		g.setLineWidth(0.05);
+		
 		//messing around with the drawing constructors
+		/*Draws a rectangle with 4 lines
 		g.strokeLine(2, 3, 2, 4);
 		g.strokeLine(2, 3, 3, 3);
 		g.strokeLine(3, 3, 3, 4);
 		g.strokeLine(2, 4, 3, 4);
 		g.strokeLine(0, 0, 0, 0);
+		*/
+		
+		/*	
+		//Draws 2 squares originating from 0,0
+		g.strokeRect(0, 0, 1, 1);
+		g.strokeRect(0, 0, 2, 2);
+		*/
+		
+		//draws four squares around the point passed in
+		fourSquares(0, 0, g);
+		fourSquares(-3, -3, g);
+		fourSquares(3, 3, g);		
+		fourSquares(-3, 3, g);
+		fourSquares(3, -3, g);
 		
 		//adds the already-drawn-on canvas to the screen
 		grid.add(canvas, 0, 2);
+	}
+
+	private void fourSquares(int startX, int startY, GraphicsContext g) {
+		g.strokeRect(startX - 1, startY - 1, 1, 1);
+		g.strokeRect(startX - 1, startY - 1, 2, 2);
+		g.strokeRect(startX, startY, 1, 1);		
 	}
 
 
