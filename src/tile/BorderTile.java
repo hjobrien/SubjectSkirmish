cb6f414f125ae1c0b59776e3c5fb807164872ed8
Liam Kreiss <liam.kreiss@gmail.com>
2015-10-22 09:18:54 -0700

    --fixed bordertile

--- src/tile/BorderTile.java
+++ src/tile/BorderTile.java
@@ -3,18 +3,12 @@ package tile;
 import graphics.Event;
 import javafx.scene.paint.Color;
 
-public class BorderTile extends Tile implements Stepable{
+public class BorderTile extends Tile {
 	
 	private static final Color c = Color.PURPLE;
 
 	public BorderTile(int x, int y) {
 		super(x, y, c);
-	}
-
-	@Override
-	public Event onStep() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
