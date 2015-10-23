e5bd3d17ad696a9d6e4c636b6c26863ceff9d4fc
Liam Kreiss <liam.kreiss@gmail.com>
2015-10-23 11:24:26 -0700

    looks at what we can do with items

--- src/event/FindItem.java
+++ src/event/FindItem.java
@@ -1,8 +1,19 @@
 package event;
 
+import java.util.Random;
+
 public class FindItem implements Event {
 
 	public FindItem(){
 		System.out.println("Item Found");
+		Random r = new Random();
+		int itemChance = r.nextInt(10);
+		if (itemChance == 0){
+			System.out.println("You found Eli");
+		} else if (itemChance < 3){
+			System.out.println("You found Akaash");
+		} else if (itemChance < 10){
+			System.out.println("You found Ben");
+		}
 	}
 }
