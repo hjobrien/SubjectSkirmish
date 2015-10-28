f795136e22daea12b15599b568b7ced2bf9398e9
Hank <hank.obrien900@icloud.com>
2015-10-27 23:01:19 -0700

    Added some comments. Also: I was confused by what a 'move' was, so i changed moves to attacks, i think its more clear.

--- src/creature/Creature.java
+++ src/creature/Creature.java
@@ -1,14 +1,21 @@
 package creature;
 
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+
 import battle.Attack;
 import battle.AttackWeightMatrix;
+import player.Item;
 
 
 public abstract class Creature {
+	
+	private BufferedImage selfPic; //should set to a default 'img. not found' picture
 	private int health;
 	private ArrayList<Attack> moves;
+	private ArrayList<Item> possibleDrops;
+	
 	public static final double[][] ATTACK_WEIGHT_MATRIX = AttackWeightMatrix.ATTACK_WEIGHT_MATRIX;
 		
 	public abstract void onDeath(); //return Event?
