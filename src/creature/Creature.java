c9147bf2d9aa8cb46bc03fdf7d1be42320ecf9ea
Hank <hank.obrien900@icloud.com>
2015-10-27 22:54:06 -0700

    Added inheritance strcutre for monsters, added general features to monsters. Added attacking weights

--- src/creature/Creature.java
+++ src/creature/Creature.java
@@ -1,8 +1,23 @@
 package creature;
 
-import java.util.ArrayList;
 
-public class Creature {
+import java.util.ArrayList;
+import battle.Attack;
+import battle.AttackWeightMatrix;
+
+
+public abstract class Creature {
 	private int health;
-	private ArrayList<Move> moves;
+	private ArrayList<Attack> moves;
+	public static final double[][] ATTACK_WEIGHT_MATRIX = AttackWeightMatrix.ATTACK_WEIGHT_MATRIX;
+		
+	public abstract void onDeath(); //return Event?
+	
+	protected void takeDamage(double damage) {
+		health -= damage;
+		if(health <= 0){
+			onDeath();
+		}
+		
+	}
 }
--- src/creature/Tanimonster.java
+++ src/creature/Tanimonster.java
@@ -1,5 +1,33 @@
 package creature;
 
-public class Tanimonster extends Creature{
+import battle.Attack;
+import battle.Type;
 
+
+public class Tanimonster extends Creature implements Enemy{
+
+	public static final Type type = Type.COMPUTER_SCIENCE;
+	
+	//TODO: initialize (init) local health in constructor to some default value, scale based on difficulty?
+	
+	@Override
+	public void onAttack(Attack a) {
+		super.takeDamage(a.getBaseDamage() * ATTACK_WEIGHT_MATRIX[a.getType().getValue()][type.getValue()]);
+		
+	}
+
+	@Override
+	public void onDeath() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void performAttack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
