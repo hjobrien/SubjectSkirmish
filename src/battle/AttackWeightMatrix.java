fdc9b35a9226c3777135d6883011a5e15f053331
Liam Kreiss <liam.kreiss@gmail.com>
2015-10-28 18:33:09 -0700

    changed the battle matrix

--- src/battle/AttackWeightMatrix.java
+++ src/battle/AttackWeightMatrix.java
@@ -8,15 +8,27 @@ public class AttackWeightMatrix {
 	//need to decide which side is attack type, and which is target type
 	
 	public static final double[][] ATTACK_WEIGHT_MATRIX = new double[][]{
-		
+						//Defense
 /*				Lit   Math  Sci   Hist.  CS	*/
-		
-/*   Lit  */	{1,    1,    1,    1,    1},
-/*   Math */	{1,    1,    1,    1,    1},
-/*   Sci  */	{1,    1,    1,    1,    1},
-/*   Hist */	{1,    1,    1,    1,    1},
-/*   CS   */	{1,    1,    1,    1,    1}
+
+	//Attack	
+/*   Lit  */	{1.0,    2.0,    2.0,    0.5,    0.5},
+/*   Math */	{0.5,    1.0,    2.0,    2.0,    0.5},
+/*   Sci  */	{2.0,    0.5,    1.0,    2.0,    0.5},
+/*   Hist */	{2.0,    2.0,    0.5,    1.0,    0.5},
+/*   CS   */	{2.0,    2.0,    2.0,    2.0,    2.0}
 
 	};
 	
 }
+
+/*
+					Defense
+			Lit   Math  Sci   Hist.  CS	
+Attack	
+   Lit  	{1,    1,    1,    1,    1},   
+   Math 	{1,    1,    1,    1,    1},
+   Sci  	{1,    1,    1,    1,    1},
+   Hist 	{1,    1,    1,    1,    1},
+   CS   	{1,    1,    1,    1,    1}
+*/
