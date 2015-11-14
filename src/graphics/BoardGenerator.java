ff3d7e29c6a613a2449d3b2fd68b2f40dd2de935
Hank <hank.obrien900@icloud.com>
2015-11-14 13:29:37 -0800

    Added a grouping algorithm for board generation

--- src/graphics/BoardGenerator.java
+++ src/graphics/BoardGenerator.java
@@ -24,6 +24,9 @@ public class BoardGenerator {
 	private static final String GRASS_PATH = "/images/GrassTile.jpeg";
 	private static final String WATER_PATH = "/images/WaterTile.jpeg";
 	private static final String BORDER_PATH = "/images/BorderTile.jpeg";
+	private static final double WATER_PRESSURE = 0.17;
+	private static final double GRASS_PRESSURE = 0.185;
+	private static final double FIRE_PRESSURE = 0.2;
 	
 	
 	public BoardGenerator(){
@@ -34,10 +37,103 @@ public class BoardGenerator {
 		switch(style){
 			case FULL_RANDOM:
 				return genRandBoard();
+			case SMALL_GROUPS:
+				return smallGroupedBoard();
 			default:
 				break;
 		}
 		return null;
+	}
+
+	private static Board smallGroupedBoard() {
+		return increaseBoardGrouping(genRandBoard());
+	}
+
+	//presure in context of evolutionary pressure
+	private static Board increaseBoardGrouping(Board seed){
+		Random rand = new Random();
+		ArrayList<ArrayList<Tile>> seedBoard = seed.getBoard();
+		Tile north;
+		Tile east;
+		Tile south;
+		Tile west;
+		Tile center;
+		double[] tileTypePressure = new double[]{0,0,0}; //water, grass, fire pressure
+		//starts at 1,1 to avoid rows of borderTile, also why it ends at size()-1
+		for(int i = 1; i < seedBoard.size()-1; i++){
+			for(int j = 1; j < seedBoard.get(0).size()-1; j++){
+				north = seed.getBoard().get(i+1).get(j);
+				east = seed.getBoard().get(i).get(j+1);
+				south = seed.getBoard().get(i-1).get(j);
+				west = seed.getBoard().get(i).get(j-1);
+				center = seed.getBoard().get(i).get(j);
+
+				tileTypePressure = getPressure(center, north, east, south, west);
+				double d = rand.nextDouble();
+				if(d < tileTypePressure[2]){
+					seedBoard.get(i).set(j, new FireTile(i,j,FIRE_PATH));
+				}
+				else if(d < tileTypePressure[1] + tileTypePressure[2]){
+					seedBoard.get(i).set(j, new GrassTile(i,j,GRASS_PATH));	
+				}
+				else if(d < tileTypePressure[0] + tileTypePressure[1] + tileTypePressure[2]){
+					seedBoard.get(i).set(j, new WaterTile(i,j,WATER_PATH));
+				}
+			}
+		}
+		return new Board(seedBoard);
+		
+	}
+	
+	private static double[] getPressure(Tile center, Tile north, Tile east, Tile south, Tile west) {
+		double[] tileTypePressure = new double[]{0,0,0};
+		int grassCount = 0, waterCount = 0, fireCount = 0;
+		if(north instanceof WaterTile){
+			waterCount++;
+		}
+		else if (north instanceof FireTile){
+			fireCount++;
+		}
+		else if (north instanceof GrassTile){
+			grassCount++;
+		}
+		//------------------
+		if(east instanceof WaterTile){
+			waterCount++;
+		}
+		else if (east instanceof FireTile){
+			fireCount++;
+		}
+		else if (east instanceof GrassTile){
+			grassCount++;
+		}
+		//------------------
+		if(south instanceof WaterTile){
+			waterCount++;
+		}
+		else if (south instanceof FireTile){
+			fireCount++;
+		}
+		else if (south instanceof GrassTile){
+			grassCount++;
+		}
+		//-------------------
+		if(west instanceof WaterTile){
+			waterCount++;
+		}
+		else if (west instanceof FireTile){
+			fireCount++;
+		}
+		else if (west instanceof GrassTile){
+			grassCount++;
+		}
+		
+		
+		tileTypePressure[0] = WATER_PRESSURE * waterCount;
+		tileTypePressure[1] = GRASS_PRESSURE * grassCount;
+		tileTypePressure[2] = FIRE_PRESSURE * fireCount;
+		
+		return tileTypePressure;
 	}
 
 	private static Board genRandBoard() {
@@ -56,12 +152,12 @@ public class BoardGenerator {
 					//automatically makes the tile that the player starts on a grass tile
 					tempTile = new GrassTile(i, j, GRASS_PATH);
 				} else {
-					int x = rand.nextInt(6); //0-5
+					int x = rand.nextInt(8); //0-7
 					if(x < 1){ //0
 						tempTile = new FireTile(i,j, FIRE_PATH);
-					} else if (x < 4){ //1-3
+					} else if (x < 5){ //1-4
 						tempTile = new GrassTile(i,j, GRASS_PATH);
-					} else { //4-5
+					} else { //5-7
 						tempTile = new WaterTile(i,j, WATER_PATH);
 					}
 					//normal tiles can also be used
